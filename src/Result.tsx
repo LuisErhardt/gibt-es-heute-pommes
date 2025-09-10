8e0edad0d9cd8585e426b958367a83c38f3e0f15
Luis Erhardt <luis@mail.com>
2025-09-10 17:00:45 +0200

    loading

--- src/Result.tsx
+++ src/Result.tsx
@@ -3,17 +3,21 @@ import React, { useEffect, useState } from "react";
 const Result: React.FC = () => {
   const [text, setText] = useState<string>("");
   const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const loadFile = async () => {
       try {
-        const response = await fetch("/result.txt");
+        setLoading(true);
+        const response = await fetch("result.txt");
         const data = await response.text();
         if (data.startsWith("<!DOCTYPE html>")) {
           throw new Error("Datei nicht gefunden (Fallback geladen)");
         }
+        setLoading(false);
         setText(data);
       } catch (err) {
+        setLoading(false);
         setError("Keine Infos zu Pommes gefunden :(");
         console.error(err);
       }
@@ -23,7 +27,15 @@ const Result: React.FC = () => {
   }, []);
 
   if (error) {
-    return <div className="text-red-500 font-bold">{error}</div>;
+    return (
+      <div className="text-red-500 border-red-600  text-5xl font-bold w-fit bg-white p-10 border-4 rounded-md">
+        {error}
+      </div>
+    );
+  }
+
+  if (loading) {
+    return <></>;
   }
   return (
     <div
